3fa68ea431aeb2c07e750c649ec61c2455b77df5
davidkatz <15Dkatz@shcp.edu>
2017-05-17 15:31:18 -0700

    add chapter1

---
+++ chapter1/c2f.rb
@@ -0,0 +1,11 @@
+celsius = 100
+fahrenheit = (celsius*9/5) + 32
+print "The result is: "
+print fahrenheit
+puts "."
+
+##
+# ruby -cw c2f.rb
+# -c checks for syntax errors.
+# The -w flag activates a higher level of warning:
+# If you've done somethinglegal, but questionable in terms of syntax.
---
+++ chapter1/loading/program.rb
@@ -0,0 +1,3 @@
+puts "This is the master program file."
+load "loadee.rb"
+puts "And back to the first file."
